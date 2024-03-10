b9315e48c3266489f5772e4f814d00c05c39884a
Felipe Vieira Lima <felipe.lima@inter.co>
2024-03-09 22:02:19 -0300

    Minor Change - Add user info component

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <h1>App page</h1>
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import { auth } from "@/services/auth"
+import { signOut } from "next-auth/react"
+import { UserInfo } from "./_components/user-info"
+
+export default async function Page() {
+  const session = await auth()
+
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <UserInfo user={session?.user}/>
+    </main>
+  )
 }
